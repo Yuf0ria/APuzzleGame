f55483e224a4d5ea354360f914e7233556277b09
maia <91291v@gmail.com>
2025-07-28 14:50:31 +0800

    call stars

---
+++ Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using static UnityEngine.ParticleSystem;
+
+public class PlayerControls : MonoBehaviour
+{
+    private ParticleSystem[] callParticleSystem;
+
+    void Start()
+    {
+        GameObject[] starObjects = GameObject.FindGameObjectsWithTag("star");
+
+        callParticleSystem = new ParticleSystem[starObjects.Length];
+
+        for (int i = 0; i < starObjects.Length; i++)
+        {
+            callParticleSystem[i] = starObjects[i].GetComponent<ParticleSystem>();
+
+            if (callParticleSystem[i] == null)
+            {
+                Debug.LogWarning($"{starObjects[i].name} does not have a ParticleSystem component!");
+            }
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            foreach (ParticleSystem ps in callParticleSystem)
+            {
+                if (ps != null)
+                {
+                    ps.Play();
+                }
+            }
+        }
+    }
+}
