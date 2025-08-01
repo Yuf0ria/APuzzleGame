222b9cd39283a750c0ef648c01300678087918c8
maia <91291v@gmail.com>
2025-08-01 12:10:09 +0800

    lvl 1 map

---
+++ Assets/Scripts/MoveCamera.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class MoveCamera : MonoBehaviour
+{
+    [SerializeField] GameObject virtualCam;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        virtualCam.SetActive(true);
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        virtualCam.SetActive(false);
+    }
+}
