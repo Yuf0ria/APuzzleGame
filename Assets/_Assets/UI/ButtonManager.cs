faae844b78d3fe843b08be9dffd14731c708558e
Soba <99894854+shysoba@users.noreply.github.com>
2025-08-05 20:43:34 +0800

    main menu, ui buttons fixed, audio added

---
+++ Assets/_Assets/UI/ButtonManager.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour
+{
+
+    public GameObject settingsMenu, mainMenuScreen;
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Main Scene");
+        Time.timeScale = 1;
+        string currentScene = UnityEngine.SceneManagement.SceneManager.GetActiveScene().name;
+    }
+
+    public void OpenSettings()
+    {
+        settingsMenu.SetActive(true);
+        mainMenuScreen.SetActive(false);
+        Time.timeScale = 0;
+    }
+    public void Home()
+    {
+        SceneManager.LoadScene("01");
+        Time.timeScale = 1;
+    }
+
+    public void Resume()
+    {
+        settingsMenu.SetActive(false);
+        Time.timeScale = 1;
+
+    }
+    public void Close()
+    {
+        settingsMenu.SetActive(false);
+        mainMenuScreen.SetActive(true);
+    }
+    public void Exit()
+    {
+        Debug.Log("Game exited");
+        Application.Quit();
+    }
+
+
+    public void RestartScene()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
