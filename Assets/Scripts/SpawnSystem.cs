410dbb0da4c48b97c2f1693b8f0bef59582a61a7
maia <91291v@gmail.com>
2025-08-08 01:01:01 +0800

    02 scene

--- Assets/Scripts/SpawnSystem.cs
+++ Assets/Scripts/SpawnSystem.cs
@@ -3,10 +3,16 @@ using UnityEngine;
 public class SpawnSystem : MonoBehaviour
 {
     [SerializeField] GameObject enemies;
+    [SerializeField] GameObject enemies1;
+    [SerializeField] GameObject enemies2;
+    [SerializeField] GameObject enemies3;
 
     private void Start()
     {
         enemies.SetActive(false);
+        enemies1.SetActive(false);
+        enemies2.SetActive(false);
+        enemies3.SetActive(false);
     }
 
     private void Update()
@@ -14,6 +20,9 @@ public class SpawnSystem : MonoBehaviour
         if(ActivateStar.enemyCanSpawn == true)
         {
             enemies.SetActive(true);
+            enemies1.SetActive(true);
+            enemies2.SetActive(true);
+            enemies3.SetActive(true);
 
         }
     }
